dc180be806670fc295b1f3a2eb1a2bfb6375cc44
faq322 <34307377+faq322@users.noreply.github.com>
2020-05-19 22:30:20 +0300

    pop info

--- Assets/Scripts/MainScripts/Shop/ShopInfoText.cs
+++ Assets/Scripts/MainScripts/Shop/ShopInfoText.cs
@@ -26,8 +26,11 @@ public class ShopInfoText : MonoBehaviour
     IEnumerator Disappear(Vector3 a, string _text)
     {
         var rotation = Quaternion.Euler(0, 0, 0);
-        GameObject h = Instantiate(shopInfotext, shopInfotext.transform.position, rotation);
-        h.transform.parent = this.transform;
+        GameObject h = Instantiate(this.shopInfotext, shopInfotext.transform.position, rotation);
+        h.transform.localScale = shopInfotext.transform.localScale;
+        //h.transform.parent = this.transform;
+        h.transform.SetParent(this.transform, false);
+        h.transform.localScale = new Vector3(1,1,1);
         h.GetComponent<Text>().text = _text;
         h.SetActive(true);
         yield return new WaitForSeconds(3f);
